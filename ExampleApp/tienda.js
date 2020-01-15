78ee70be5fded11d81ba393e7a4653712b556bc4
maurigonzz <maurigonzz@gmail.com>
2020-01-15 00:04:20 -0300

    Update tienda.js

--- ExampleApp/tienda.js
+++ ExampleApp/tienda.js
@@ -1,3 +1,11 @@
-(function(){
-    console.log("Hello world!");
+(function () {
+        var options = {
+            whatsapp: "+59899203412", // WhatsApp number
+            call_to_action: "", // Call to action
+            position: "right", // Position may be 'right' or 'left'
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
 })();
