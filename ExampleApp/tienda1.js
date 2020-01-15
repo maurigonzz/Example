8e89f20c3171cae7a99dc36dfa7e4689ada79247
maurigonzz <maurigonzz@gmail.com>
2020-01-15 20:39:29 -0300

    Update tienda1.js

--- ExampleApp/tienda1.js
+++ ExampleApp/tienda1.js
@@ -2,7 +2,6 @@
     myButton = document.createElement("input");
     myButton.type = "button";
     myButton.value = "Pagar con Tarjeta";
-    myButton.classList.add("btn btn-primary btn-submit ladda-button m-half-top");
     placeHolder = document.getElementsByClassName("custom-container");
     placeHolder[0].appendChild(myButton);
     console.log("Hello world! con tarjeta");
