ba497f6a993b80d44df35e9627310d2331e052fa
maurigonzz <maurigonzz@gmail.com>
2020-01-25 16:53:44 -0300

    Update and rename app.js to app2.js

--- ExampleApp/app2.js
+++ ExampleApp/app2.js
@@ -1,7 +1,7 @@
 (function () {
 
     var loadScript = function(url, callback){
-
+            var script = document.createElement("script")
             script.type = "text/javascript";
 
             if (script.readyState) { //IE
