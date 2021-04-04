4adaef5d0c1b92d658ae6c78ee425bf92fee54bf
maurigonzz <maurigonzz@gmail.com>
2021-04-04 20:47:39 -0300

    Create sliderImages.js

---
+++ ExampleApp/sliderImages.js
@@ -0,0 +1,6 @@
+(function(){
+    sliderImages = document.getElementsByClassName("slider-image");
+    for (var i = 0; i < sliderImages.length; i++) {
+      sliderImages[i].style.height = "auto";
+    }
+})();
