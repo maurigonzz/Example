848d99a5ea9613457c1987909ebf92098721af6e
maurigonzz <maurigonzz@gmail.com>
2021-04-04 21:39:34 -0300

    Create sectionSlider.js

---
+++ ExampleApp/sectionSlider.js
@@ -0,0 +1,7 @@
+
+(function(){
+    sectionSlider = document.getElementsByClassName("section-slider");
+    for (var i = 0; i < sectionSlider.length; i++) {
+      sectionSlider[i].style.height = "auto";
+    }
+})();
